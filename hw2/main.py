cfaef2b65c32c110d653f5a0e1e2f7ca7a09290b
Itai Spigelman <itai10020@gmail.com>
2022-11-19 11:17:05 +0200

    hw2 rk2,rk4

--- hw2/main.py
+++ hw2/main.py
@@ -1,24 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from numalg import euler_method
+from numalg import euler_method, rk2_method, rk4_method
 
 def harmonic_oscillator_euler(T, N, v0, x0):
     return euler_method(T, N, lambda x: np.array([x[1], -x[0]]), np.array([x0, v0]))
 
 def harmonic_oscillator_rk2(T, N, v0, x0):
-    pass
+    return rk2_method(T, N, lambda x: np.array([x[1], -x[0]]), np.array([x0, v0]))
 
 def harmonic_oscillator_rk4(T, N, v0, x0):
-    pass
+    return rk4_method(T, N, lambda x: np.array([x[1], -x[0]]), np.array([x0, v0]))
 
 T = 2 * np.pi
-N = 20
+N = 15
 
-x = harmonic_oscillator_euler(T, N, 0, 1)
+methods = [('euler', harmonic_oscillator_euler),
+           ('RK2', harmonic_oscillator_rk2),
+           ('RK4', harmonic_oscillator_rk4)]
 
-plt.plot(np.linspace(0, T, N+1), x[:, 0])
+for method_name, method_func in methods:
+    x = method_func(T, N, 0, 1)
+    plt.plot(np.linspace(0, T, N + 1), x[:, 0], '.', label=method_name)
+
 plt.xlabel(r't $\left[ sec \right]$')
 plt.ylabel(r'x $\left[ m \right]$')
+plt.legend()
 plt.grid()
+plt.savefig('numeric_harmonic_oscillator.png')
 plt.show()
+
+
--- hw2/numalg.py
+++ hw2/numalg.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def euler_step(x, f, tau):
+    return x + tau * f(x)
+
 def euler_method(T, steps, f, x0):
     tau = T / steps
     d = len(x0)
@@ -7,6 +10,33 @@ def euler_method(T, steps, f, x0):
     x[0] = x0
 
     for n in range(0, steps):
-        x[n+1] = x[n] + tau*f(x[n])
+        x[n+1] = euler_step(x[n], f, tau)
 
     return x
+
+def rk2_method(T, steps, f, x0):
+    tau = T / steps
+    d = len(x0)
+    x = np.zeros((steps + 1, d))
+    x[0] = x0
+
+    for n in range(0, steps):
+        x[n + 1] = x[n] + tau * f(euler_step(x[n], f, tau/2))
+
+    return x
+
+def rk4_method(T, steps, f, x0):
+    tau = T / steps
+    d = len(x0)
+    x = np.zeros((steps + 1, d))
+    x[0] = x0
+
+    for n in range(0, steps):
+        F1 = f(x[n])
+        F2 = f(x[n] + (1/2)*tau*F1)
+        F3 = f(x[n] + (1/2)*tau*F2)
+        F4 = f(x[n] + tau*F3)
+        x[n + 1] = x[n] + (1/6)*tau*(F1+2*F2+2*F3+F4)
+
+    return x
+
